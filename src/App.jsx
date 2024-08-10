e8cbfe7f6831524fa0fc0edefd378d0f8b829708
Alka Sah <alkasah485@gmail.com>
2024-08-10 01:52:38 -0700

    modification in header

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,19 @@
 import styled from 'styled-components'
 import axios from 'axios'
 //---------------------------------------------------------import dependency
+import {Header, AppNameComponent} from './components/Header'
 import './App.css'
 
 function App() {
   
   return (
     <>
+    <Header>
+      <AppNameComponent>
+      <p>Hello</p>
+      </AppNameComponent>
+
+    </Header>
       
     </>
   )
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,10 +1,18 @@
 /* eslint-disable no-unused-vars */
-import React from 'react'
+import styled from 'styled-components'
 
-const Header = () => {
-  return (
-    <div>Header</div>
-  )
-}
+export const Header = styled.div`
+       color:white;
+       background-color: black;
+       display:flex;
+       padding: 20px;
+       flex-direction:row;
+       align-items:center;
+       justify-content:space-between;
+       font-size: 25px;
+       font-weight: bold;
+       box-shadow: 0 3px 6px 0 #555;
+`;
 
-export default Header
+export const AppNameComponent = styled.div`
+`;
