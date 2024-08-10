5608ef5d1f4668f64d2de5e4edac8947c549ac3a
Alka Sah <alkasah485@gmail.com>
2024-08-10 01:27:36 -0700

    added dependency

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,7 @@
-
+/* eslint-disable no-unused-vars */
+import styled from 'styled-components'
+import axios from 'axios'
+//---------------------------------------------------------import dependency
 import './App.css'
 
 function App() {
