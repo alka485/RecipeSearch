4d3e59e35a4c1232109c150d2fbeeae71b846382
Alka Sah <alkasah485@gmail.com>
2024-08-17 13:33:09 -0700

    modification saved

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,38 @@
 /* eslint-disable no-unused-vars */
 import styled from 'styled-components'
 import axios from 'axios'
+import { useState } from 'react'
 //---------------------------------------------------------import dependency
 import {Header, AppNameComponent,AppIcon,SearchComponent,SearchIcon,SearchInput} from './components/Header'
 import './App.css'
 
 function App() {
+    const [timeoutId, settimeoutId] = useState();
+    const [recipeList, setRecipeList] = useState([]);
 
   const fetchRecipe = async (searchString) => {
-    const response = await axios.get(`https://api.edamam.com/search?q=${searchString}&app_id=${import.meta.env.VITE_APP_ID }&app_key=${import.meta.env.VITE_APP_KEY}`);
-    console.log(response);
-    
+    // console.log(searchString);
+    // console.log(import.meta.env.VITE_APPID, import.meta.env.VITE_APPKEY);
+    try{
+      const response = await axios.get(`https://api.edamam.com/search?q=${searchString}&app_id=${import.meta.env.VITE_APPID}&app_key=${import.meta.env.VITE_APPKEY}`);
+      //console.log(response);
+      setRecipeList(response.data)
 
+    } catch (err) {
+      console.error(err);
+      
+    }
   }
 
   const handleChange =(e) => {
-    console.log(e.target.value);
-    
-  }
+    clearTimeout(timeoutId);//Clear the previous timeout if it exists
+    const searchString = e.target.value;
+    // Set a new timeout to fetch the recipe after 500ms of inactivity
+    const newTimeout = setTimeout(() => {
+      fetchRecipe(searchString);//Make API call with the search string
+    }, 500);
+    settimeoutId(newTimeout);//Store the new timeout ID
+  };
   
   return (
     <>
@@ -27,7 +42,7 @@ function App() {
         Recipe Search
       </AppNameComponent>
       <SearchComponent>
-        <SearchIcon src='../public/search-icon.svg'/>
+        <SearchIcon src='/search-icon.svg'/>
         <SearchInput placeholder='Search Recipe' onChange={handleChange}/>
         
 
